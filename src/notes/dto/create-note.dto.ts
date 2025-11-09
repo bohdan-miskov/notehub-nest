ec3e171d60327964fc4c4680e35d0f3cd9ff5b3a
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-09 09:40:39 +0200

    Created note validator

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,1 +1,28 @@
-export class CreateNoteDto {}
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { NoteTag } from '../enums/note-tag.enum';
+
+export class CreateNoteDto {
+  @IsString({ message: 'Title must be a string' })
+  @IsNotEmpty({ message: 'Title cannot be empty' })
+  @MinLength(3, { message: 'Title must be at least 3 characters long' })
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isDone?: boolean;
+
+  @IsEnum(NoteTag)
+  @IsOptional()
+  tag?: NoteTag;
+}
