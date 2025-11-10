0cab2cb22bdb1f26ab9a19f0bf015120a9f05d12
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-10 14:29:58 +0200

    Created AuthModule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { SessionsModule } from 'src/sessions/sessions.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    UsersModule,
+    SessionsModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({}),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
