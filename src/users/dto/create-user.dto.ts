f77b4f1e2250615d2968836ed5d1e94f642d109b
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-11 18:52:56 +0200

    Added swagger desc for users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Bohdan',
+    description: "User's first name",
+  })
   name: string;
+
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'The unique email of the user',
+  })
   email: string;
+
+  @ApiProperty({
+    example: 'Str0ngP@ss!',
+    description: 'User password (at least 8 characters)',
+  })
   password: string;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty({
+    example: 'Bohdan',
+    description: "User's first name",
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
