274e3d7e094d93c90921c5640aaca2a51fc2e4f1
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-11 18:51:27 +0200

    Added endpoint for docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,17 @@ async function bootstrap() {
   );
 
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('NoteHun API')
+    .setDescription('REST API documentation for the NoteHub project')
+    .setVersion('1.0')
+    .addCookieAuth('accessToken')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(+(process.env.PORT ?? 3000));
 }
