169bb7930611ba4d8ca06dde0331280d761186e6
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-11 20:41:23 +0200

    Added docs info for PaginationQueryDro

--- src/common/dto/pagination-query.dto.ts
+++ src/common/dto/pagination-query.dto.ts
@@ -1,13 +1,22 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsInt, IsOptional, Min } from 'class-validator';
 
 export class PaginationQueryDto {
+  @ApiPropertyOptional({
+    example: 1,
+    description: 'Number of page',
+  })
   @IsOptional()
   @IsInt()
   @Min(1)
   @Transform(({ value }: { value: string }) => parseInt(value, 10))
   page: number = 1;
 
+  @ApiPropertyOptional({
+    example: 10,
+    description: 'Count of elements in page',
+  })
   @IsOptional()
   @IsInt()
   @Min(1)
