ab2962c1cb010fc2be64bf22fa753f8c4bbe573b
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-11 20:43:06 +0200

    Added script for receiving docs .json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import fs from 'node:fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,14 +18,31 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   const config = new DocumentBuilder()
-    .setTitle('NoteHun API')
+    .setTitle('NoteHub API')
     .setDescription('REST API documentation for the NoteHub project')
     .setVersion('1.0')
-    .addCookieAuth('accessToken')
+    .addTag('1. Auth', 'User authentication and session management')
+    .addTag('2. Notes', 'Operations for notes')
+    .addTag('3. Users', 'User profile operations')
+    .addSecurity('accessToken', {
+      type: 'apiKey',
+      in: 'cookie',
+      name: 'accessToken',
+    })
+    .addSecurity('refreshToken', {
+      type: 'apiKey',
+      in: 'cookie',
+      name: 'refreshToken',
+    })
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
+  // For docs checking
+  // await fs.writeFile('docs.json', JSON.stringify(document));
 
   SwaggerModule.setup('docs', app, document, {
     swaggerOptions: {
