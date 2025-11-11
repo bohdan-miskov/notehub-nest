b0a6915db9f5323f6c5117392533f279e39b3d7b
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-11 19:00:37 +0200

    Added redirect to docs for app root

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { SessionsModule } from './sessions/sessions.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -20,11 +19,6 @@ import { SessionsModule } from './sessions/sessions.module';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         url: configService.getOrThrow<string>('DATABASE_URL'),
-        // host: process.env.DB_HOST,
-        // port: +(process.env.DB_PORT ?? 3306),
-        // username: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: false,
         ssl: {
@@ -32,12 +26,11 @@ import { SessionsModule } from './sessions/sessions.module';
         },
       }),
     }),
-    NotesModule,
     AuthModule,
+    NotesModule,
     UsersModule,
     SessionsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
