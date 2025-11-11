d071cad2b6621fc8bb552249606fa5e8d9bd4f7d
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-11 18:52:37 +0200

    Added swagger desc for auth

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,15 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({
+    example: 'Bohdan',
+    description: "User's first name",
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'The unique email of the user',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
   email: string;
 
+  @ApiProperty({
+    example: 'Str0ngP@ss!',
+    description: 'User password (at least 8 characters)',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
