d277d4b04389ddae0994e31209596d2586810c19
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-19 10:31:05 +0200

    Created register test for AuthService

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,154 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { MockService } from 'src/common/types/mock.types';
+import { UsersService } from 'src/users/users.service';
+import { SessionsService } from 'src/sessions/sessions.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { RegisterDto } from './dto/register.dto';
+import { User } from 'src/users/entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { ConflictException } from '@nestjs/common';
+
+function mockServiceFactory<T extends object>(): MockService<T> {
+  return {
+    findOneByEmail: jest.fn(),
+    create: jest.fn(),
+    findByHash: jest.fn(),
+    removeByHash: jest.fn(),
+    signAsync: jest.fn(),
+    get: jest.fn(),
+  } as MockService<T>;
+}
+
+function mockUserFactory(props: Partial<User> = {}): User {
+  return {
+    id: 1,
+    name: 'testUser',
+    email: 'test@example.com',
+    password: '$2b$10$C8iH2iwkHWPbD7CMer/ruOmOGRcMmdSmzA3thShgNO97fGbnd93pW',
+    notes: [],
+    sessions: [],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...props,
+  };
+}
+
+jest.mock('bcrypt', () => ({
+  hash: jest.fn(),
+  compare: jest.fn(),
+}));
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  let mockUsersService: MockService<UsersService>;
+  let mockSessionsService: MockService<SessionsService>;
+  let mockJwtService: MockService<JwtService>;
+  let mockConfigService: MockService<ConfigService>;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: mockServiceFactory<UsersService>(),
+        },
+        {
+          provide: SessionsService,
+          useValue: mockServiceFactory<SessionsService>(),
+        },
+        {
+          provide: JwtService,
+          useValue: mockServiceFactory<JwtService>(),
+        },
+        {
+          provide: ConfigService,
+          useValue: mockServiceFactory<ConfigService>(),
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    mockUsersService = module.get(UsersService);
+    mockSessionsService = module.get(SessionsService);
+    mockJwtService = module.get(JwtService);
+    mockConfigService = module.get(ConfigService);
+
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should successfully register a new user', async () => {
+      const hashedPassword = 'mockedHashedPassword';
+      const accessToken = 'access_token';
+      const refreshToken = 'refresh_token';
+
+      (bcrypt.hash as jest.Mock).mockResolvedValue(hashedPassword);
+
+      mockUsersService.findOneByEmail?.mockResolvedValue(null);
+
+      const registerDto: RegisterDto = {
+        name: 'Test user',
+        email: 'user@example',
+        password: 'password',
+      };
+
+      const mockUser = mockUserFactory({
+        ...registerDto,
+        password: hashedPassword,
+      });
+      mockUsersService.create?.mockResolvedValue(mockUser);
+
+      mockConfigService.get?.mockReturnValueOnce(null);
+
+      mockJwtService.signAsync?.mockResolvedValueOnce(accessToken);
+      mockJwtService.signAsync?.mockResolvedValueOnce(refreshToken);
+
+      const result = await service.register(registerDto);
+      expect(result).toEqual({
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      });
+
+      expect(bcrypt.hash).toHaveBeenCalledWith(registerDto.password, 10);
+
+      expect(mockUsersService.findOneByEmail).toHaveBeenCalledWith(
+        registerDto.email,
+      );
+      expect(mockUsersService.create).toHaveBeenCalledWith({
+        ...registerDto,
+        password: hashedPassword,
+      });
+      expect(mockSessionsService.create).toHaveBeenCalledWith({
+        user: mockUser,
+        refreshTokenHash: expect.any(String) as string,
+        expiresAt: expect.any(Date) as Date,
+      });
+    });
+    it('should throw ConflictException if user already exist', async () => {
+      const registerDto: RegisterDto = {
+        name: 'mockUser',
+        email: 'user@mock.com',
+        password: 'password',
+      };
+
+      const mockUser = mockUserFactory({ email: registerDto.email });
+      mockUsersService.findOneByEmail?.mockResolvedValue(mockUser);
+
+      await expect(service.register(registerDto)).rejects.toThrow(
+        ConflictException,
+      );
+
+      expect(mockUsersService.findOneByEmail).toHaveBeenCalledWith(
+        registerDto.email,
+      );
+    });
   });
 });
