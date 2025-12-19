7c8330a9f88bd8884b09e84e9bc17232c43dcf6f
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-19 11:34:00 +0200

    Created logout and refreshTokens tests for AuthService

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -8,8 +8,14 @@ import { ConfigService } from '@nestjs/config';
 import { RegisterDto } from './dto/register.dto';
 import { User } from 'src/users/entities/user.entity';
 import * as bcrypt from 'bcrypt';
-import { ConflictException, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Session } from 'src/sessions/entities/session.entity';
+import { addDays } from 'src/common/utils/addDays';
 
 function mockServiceFactory<T extends object>(): MockService<T> {
   return {
@@ -32,6 +38,17 @@ function mockUserFactory(props: Partial<User> = {}): User {
     sessions: [],
     createdAt: new Date(),
     updatedAt: new Date(),
+    ...props,
+  };
+}
+
+function mockSessionFactory(props: Partial<Session> = {}): Session {
+  return {
+    id: 1,
+    user: { id: 1 } as User,
+    refreshTokenHash: 'hash',
+    expiresAt: addDays(7),
+    createdAt: new Date(),
     ...props,
   };
 }
@@ -170,7 +187,7 @@ describe('AuthService', () => {
       mockUsersService.findOneByEmail?.mockResolvedValue(mockUser);
 
       (bcrypt.compare as jest.Mock).mockResolvedValue(true);
-      mockConfigService.get?.mockReturnValue(null);
+      mockConfigService.get?.mockReturnValue(7);
 
       mockJwtService.signAsync?.mockResolvedValueOnce(accessToken);
       mockJwtService.signAsync?.mockResolvedValueOnce(refreshToken);
@@ -233,6 +250,83 @@ describe('AuthService', () => {
       expect(bcrypt.compare).toHaveBeenCalledWith(
         loginDto.password,
         mockUser.password,
+      );
+    });
+  });
+  describe('logout', () => {
+    it('should successfully logout user', async () => {
+      const refreshToken = 'refreshToken';
+
+      const result = await service.logout(refreshToken);
+      expect(result).toEqual(undefined);
+
+      expect(mockSessionsService.removeByHash).toHaveBeenCalledWith(
+        expect.any(String) as string,
+      );
+    });
+  });
+  describe('refreshToken', () => {
+    it('should successfully refresh user tokens', async () => {
+      const refreshToken = 'refreshToken';
+      const newRefreshToken = 'newRefreshToken';
+      const newAccessToken = 'newAccessToken';
+
+      const mockSession = mockSessionFactory();
+      mockSessionsService.findByHash?.mockResolvedValue(mockSession);
+
+      mockConfigService.get?.mockReturnValue(7);
+
+      mockJwtService.signAsync?.mockResolvedValueOnce(newAccessToken);
+      mockJwtService.signAsync?.mockResolvedValueOnce(newRefreshToken);
+
+      const result = await service.refreshTokens(refreshToken);
+      expect(result).toEqual({
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken,
+      });
+
+      expect(mockSessionsService.findByHash).toHaveBeenCalledWith(
+        expect.any(String) as string,
+      );
+      expect(mockSessionsService.removeByHash).toHaveBeenCalledWith(
+        expect.any(String) as string,
+      );
+      expect(mockSessionsService.create).toHaveBeenCalledWith({
+        user: mockSession.user,
+        refreshTokenHash: expect.any(String) as string,
+        expiresAt: expect.any(Date) as Date,
+      });
+    });
+    it('should throw ForbiddenException if refreshToken is not correct', async () => {
+      const refreshToken = 'refreshToken';
+
+      mockSessionsService.findByHash?.mockResolvedValue(null);
+
+      await expect(service.refreshTokens(refreshToken)).rejects.toThrow(
+        ForbiddenException,
+      );
+
+      expect(mockSessionsService.findByHash).toHaveBeenCalledWith(
+        expect.any(String) as string,
+      );
+    });
+    it('should throw ForbiddenException if refreshToken expired', async () => {
+      const refreshToken = 'refreshToken';
+      const mockSession = mockSessionFactory({
+        expiresAt: addDays(-1),
+      });
+
+      mockSessionsService.findByHash?.mockResolvedValue(mockSession);
+
+      await expect(service.refreshTokens(refreshToken)).rejects.toThrow(
+        ForbiddenException,
+      );
+
+      expect(mockSessionsService.findByHash).toHaveBeenCalledWith(
+        expect.any(String) as string,
+      );
+      expect(mockSessionsService.removeByHash).toHaveBeenCalledWith(
+        expect.any(String) as string,
       );
     });
   });
