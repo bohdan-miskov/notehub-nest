9ac30cd6e678f6eb8690a9d6f61007ab23301efe
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-19 17:19:50 +0200

    Created update test for UsersController

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -4,6 +4,7 @@ import { UsersService } from './users.service';
 import { MockService } from 'src/common/types/mock.types';
 import { User } from './entities/user.entity';
 import { Request } from 'express';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 function mockServiceFactory<T extends object>(): MockService<T> {
   return {
@@ -67,6 +68,30 @@ describe('UsersController', () => {
       expect(result).toEqual(mockUser);
 
       expect(mockUsersService.findOne).toHaveBeenCalledWith(userId);
+    });
+  });
+  describe('update', () => {
+    it('should successfully update current user', async () => {
+      const updateDto: UpdateUserDto = {
+        name: 'newUser',
+      };
+      const userId = 1;
+      const mockRequest = {
+        user: {
+          id: userId,
+          ...updateDto,
+        },
+      } as Request;
+
+      const mockUser = mockUserFactory({
+        id: userId,
+      });
+      mockUsersService.update?.mockResolvedValue(mockUser);
+
+      const result = await controller.update(updateDto, mockRequest);
+      expect(result).toEqual(mockUser);
+
+      expect(mockUsersService.update).toHaveBeenCalledWith(userId, updateDto);
     });
   });
 });
