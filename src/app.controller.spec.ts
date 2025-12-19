2e880105904964f5d3da80c1346a9047b250d434
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-19 21:50:07 +0200

    Created test for AppController

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+  let controller: AppController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [],
+    }).compile();
+
+    controller = module.get<AppController>(AppController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('root', () => {
+    it('should return undefined', () => {
+      expect(controller.root()).toBeUndefined();
+    });
+  });
+});
