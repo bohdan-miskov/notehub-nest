c905f6acfc9b3c446db03ea594b30aa4e35a2e96
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-21 20:35:02 +0200

    Added util for registering user and receiving his cookies

--- test/utils/helpers.ts
+++ test/utils/helpers.ts
@@ -1,5 +1,27 @@
+import { INestApplication } from '@nestjs/common';
+import { RegisterDto } from 'src/auth/dto/register.dto';
 import request from 'supertest';
+import { App } from 'supertest/types';
 
 export const getCookies = (res: request.Response): string[] => {
   return res.headers['set-cookie'] as unknown as string[];
+};
+
+export const registerUser = async (
+  app: INestApplication,
+  registerDto: Partial<RegisterDto> = {},
+): Promise<string[]> => {
+  const uniqueId = Date.now();
+  const baseRegisterDto: RegisterDto = {
+    name: 'testUser',
+    email: `test${uniqueId}@mock.com`,
+    password: 'mockPassword',
+    ...registerDto,
+  };
+  const response = await request(app.getHttpServer() as App)
+    .post('/auth/register')
+    .send(baseRegisterDto)
+    .expect(201);
+
+  return getCookies(response);
 };
