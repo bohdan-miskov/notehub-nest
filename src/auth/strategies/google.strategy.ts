763971e0a3002095ce5da78eed0badfbafa1cef1
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-25 23:04:03 +0200

    Created oauth strategy

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(
+    private configService: ConfigService,
+    private authService: AuthService,
+  ) {
+    super({
+      clientID: configService.get<string>('GOOGLE_CLIENT_ID') as string,
+      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET')!,
+      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL')!,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback,
+  ) {
+    const { name, emails, photos } = profile;
+    const createDto = {
+      name: name?.givenName as string,
+      email: emails ? emails[0].value : '',
+    };
+    if (photos && photos[0].value) {
+      createDto['avatar'] = photos[0].value;
+    }
+
+    const user = await this.authService.validateOAuthUser(createDto);
+    done(null, user);
+  }
+}
